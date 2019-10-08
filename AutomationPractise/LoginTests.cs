66e2be444c16307884b257c0a547ebb3a834a21b
jonatandev1 <jonatan.carlsson90@gmail.com>
2019-10-08 16:21:12 +0200

    Cleaned up the code a little by adding TestCaseBase and moving test/config setup there.

---
+++ AutomationPractise/LoginTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutomationPractise
+{
+    [TestClass]
+    public class LoginTests : TestCaseBase
+    {
+        
+        //First test is up and running 1/10/19
+        [TestMethod]
+        public void VerifyPage()
+        {
+            _driver.Url = "https://ultimateqa.com/automation/";
+            _driver.Title.Contains("ultimateqa");
+        }
+
+        [TestMethod]
+        public void LoginTest()
+        {
+             
+            _driver.Url = "https://ultimateqa.com/automation/";
+            //I want to move these steps into a base class so the test has a clearer syntax
+            var login = WaitForElement(By.CssSelector(".et_pb_text_inner > ul:nth-child(2) > li:nth-child(6) > a:nth-child(1)"));
+            login.Click();
+            var name = WaitForElement(By.Id("user[email]"));
+            name.SendKeys("Test33@test.com");
+            var password = WaitForElement(By.Id("user[password]"));
+            password.SendKeys("test33");
+            var signIn = WaitForElement(By.CssSelector("div.form__button-group > input"));
+            signIn.Click();
+            _driver.Title.Equals("https://courses.ultimateqa.com/collections"); //This was added for quick verify test
+
+
+
+        }
+        
+        
+    }
+    
+}
---
+++ AutomationPractise/TestCaseBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationPractise
+{
+    public class TestCaseBase
+    {
+        public ChromeDriver _driver;
+
+        [TestInitialize]
+        public void ChromeDriverInitialize()
+        {
+            var options = new ChromeOptions
+            {
+                PageLoadStrategy = PageLoadStrategy.Normal
+            };
+            _driver = new ChromeDriver("C:/Users/jonat/source/repos/AutomationPractise/AutomationPractise/bin/Debug/netcoreapp2.1");
+        }
+        public IWebElement WaitForElement(By by, bool mustbeVisible = false, int timeOut = 10)
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeOut));
+            var element = wait.Until(d => d.FindElement(by));
+            //if (mustbeVisible) Look through this later
+            //{
+            //    wait.Until(ExpectedConditions.ElementIsVisible(by));
+            //}
+            return element;
+        }
+        [TestCleanup]
+        public void ChromeDriverCleanup()
+        {
+            _driver.Quit();
+        }
+    }
+}
